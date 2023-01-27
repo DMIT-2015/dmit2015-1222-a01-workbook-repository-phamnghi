f523e4e2ac5dab3bc591925ddf8c50072808d545
Nghi Pham <npham12@nait.ca>
2023-01-27 11:45:06 -0700

    class Jan 27

--- dmit2015-azure-webapp/src/main/java/dmit2015/model/CanadianIncomeTaxManager.java
+++ dmit2015-azure-webapp/src/main/java/dmit2015/model/CanadianIncomeTaxManager.java
@@ -1,10 +1,32 @@
 package dmit2015.model;
 
+import lombok.Getter;
+
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 public class CanadianIncomeTaxManager {
+    //Define a private constructor to implement Single pattern
+    private CanadianIncomeTaxManager(){
+
+    }
+
+    //Define a Single instance of this class
+    private static CanadianIncomeTaxManager INSTANCE;
+
+    // Define a static class-level to access the singleton
+    public static CanadianIncomeTaxManager getInstance(){
+        if(INSTANCE == null){
+            INSTANCE = new CanadianIncomeTaxManager();
+        }
+        return INSTANCE;
+    }
+
+    @Getter
+    private List<CanadianPersonalIncomeTaxRate> incomeTaxRates;  //field
     public List<String> fetchIncomeTaxData(){
         List<String> incomeTaxData = null;
         try{
@@ -13,9 +35,10 @@ public class CanadianIncomeTaxManager {
                             .getClassLoader()
                             .getResource("data/CanadianPersonalIncomeTaxRates.csv").toURI());
             incomeTaxData = Files.readAllLines(csvPath);
-        }catch(){
-            throw new Exception()
-        }
 
+        } catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
+        return incomeTaxData;
     }
 }
